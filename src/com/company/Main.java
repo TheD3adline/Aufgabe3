88f1c9d8b18d23be1971965e798cf43097ded178
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-21 18:40:25 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char letter = 't'; //Teilaufgabe a.)
+
+        char firstLetter;
+        firstLetter = 'M'; //Teilaufgabe b.)
+
+    }
+}
